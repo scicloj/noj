7d39b81c3230a831137c94eb9c9d150f2b5427e3
daslu <daniel.slutsky@gmail.com>
2022-04-01 10:10:46 +0300

    updates to viz layer

--- src/scicloj/noj/v1/viz.clj
+++ src/scicloj/noj/v1/viz.clj
@@ -4,11 +4,18 @@
             [scicloj.viz.api]
             [scicloj.kindly.v2.api :as kindly]
             [scicloj.kindly.v2.kind :as kind]
-            [scicloj.clay.v1.view]))
+            [scicloj.clay.v1.view]
+            [scicloj.clay.v1.tools]))
 
-(defn data [data]
+(defn hanami-data [data]
   (scicloj.viz.api/data data))
 
-(defn viz [& args]
+(defn hanami-plot [& args]
   (-> (apply scicloj.viz.api/viz args)
       (kindly/consider kind/vega-lite)))
+
+(defn in-iframe [hiccup]
+  [:iframe {:width       "100%"
+            :height      "500px"
+            :frameBorder "0"
+            :srcdoc      hiccup}])
