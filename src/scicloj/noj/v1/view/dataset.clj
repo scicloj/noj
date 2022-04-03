4903e2d8fcdcff31ea102f33cea8ce0732b2f0d7
daslu <daniel.slutsky@gmail.com>
2022-04-04 01:52:32 +0300

    table viewer for portal; cleanup

--- src/scicloj/noj/v1/view/dataset.clj
+++ src/scicloj/noj/v1/view/dataset.clj
@@ -3,7 +3,7 @@
             [nextjournal.clerk :as clerk]
             [scicloj.kindly.v2.api :as kindly]
             [scicloj.kindly.v2.kindness :as kindness]
-            [scicloj.clay.v1.tool.html.table :as html.table]))
+            [scicloj.clay.v1.html.table :as html.table]))
 
 
 (extend-protocol kindness/Kindness
@@ -14,20 +14,11 @@
 (kindly/define-kind-behaviour!
   :kind/dataset
   {:portal.viewer (fn [v]
-                    [:portal.viewer/hiccup
-                     [:portal.viewer/markdown (-> v
-                                                  println
-                                                  with-out-str)]])})
+                    [:portal.viewer/table
+                     (seq (tmd/mapseq-reader v))])})
+
 (kindly/define-kind-behaviour!
   :kind/dataset
   {:clerk.viewer (fn [v]
                    (clerk/table {:head (tmd/column-names v)
                                  :rows (tmd/rowvecs v)}))})
-
-(kindly/define-kind-behaviour!
-  :kind/dataset
-  {:html.viewer (fn [v]
-                  (-> {:column-names (tmd/column-names v)
-                       :row-vectors (tmd/rowvecs v)}
-                      html.table/->table-hiccup
-                      html.table/table-hiccup->datatables-html))})
