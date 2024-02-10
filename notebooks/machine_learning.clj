54ff75cc6b0e84ed80712fe3d0915881331235d3
Carsten Behring <carsten.behring@gmail.com>
2024-02-10 16:50:20 +0100

    first take on 'classification'

---
+++ notebooks/machine_learning.clj
@@ -0,0 +1,151 @@
+(ns machine-learning
+  (:require [scicloj.ml.metamorph :as ml]
+
+            [tech.v3.dataset :as ds]
+            [scicloj.kindly.v4.kind :as kind]))
+
+
+
+(comment
+  (require '[scicloj.clay.v2.api :as clay]
+           '[tech.v3.dataset.column :as col])
+
+
+
+
+  (clay/start!))
+
+;; # machine learning specific functionality in `tech.ml.dataset`
+;; The library `tech.ml.dataset` contains several functions
+;; operating on a daaset, which are mainly used in teh context of machine learining
+;; In the following we will introduce those.
+;;
+
+;;## Categorical variables
+;;One typicall problem in machine learining is `classification`,
+;;so learning how to categorize data in different categories.
+;;Sometimes data in this format is as well called "qualitative data"
+;;or data aving `discrete` values
+
+;; These categories are often expressed in Clojure the data as of
+;; being  `type` String or `keyword`
+;;
+;; In `dataset` it is the `Column` which has specific support for
+;; categorical data
+
+;; Creating a column out of categorical data like this:
+
+(def column-x (col/new-column  :x  [:a :b]))
+
+;; creates a "categorical" column, which is marked as such in
+;; the column metadata.
+
+;; Printing the var show its "type" as being `keyword`
+column-x
+;; and printint its metadata show that it got marked as `categorical`
+(meta column-x)
+
+;; The column is therefore using its metdata, and it is important to get used to look
+;; at it in case of debugging issues.
+;; The same happens, when creating a `dataset` which is at thend end a seq
+;; of columns
+;;
+(def categorical-ds
+  (ds/->dataset
+   {:x [:a :b] :y ["c" "d"]}))
+
+categorical-ds
+
+(map
+ meta
+ (vals categorical-ds))
+
+;; ### Express categorical vars in numeric space
+;; Most machine learining models can only work on numerical values,
+;; both for features and the target variable
+;; So usualy we need to transform categorical data into a numeric representation,
+;; so each category need to be converted to a number.
+;;  These numbers have no meaning for the users, so oftne we need to convert bacl into
+;;  String / keyword space
+;;
+;; Namespace `tech.ml.dataset.classification`
+;; has several functions to do so.
+;;
+;; ### Transform categorical column into a numerical column
+(require  '[tech.v3.dataset.categorical :as ds-cat])
+
+;; These function operate on a single column, but expect a dataset and
+;; a colum name as input
+;;
+;; We can ask it to find a mapping from string/keyword to a
+;; numerical space (0 ... x) like this
+
+(ds-cat/fit-categorical-map categorical-ds :x)
+
+;; This maps value in the order of occurence in the column to
+;; 0 .. 1
+;; This is a bit dangerous, as the mapping is decided by "row order",
+;; which could change or be different on other subset of the data
+;; So it is prefered to be specified explicitely
+
+(def x-mapping (ds-cat/fit-categorical-map categorical-ds :x [:a :b]))
+x-mapping
+;;  Now we know for sure, that :a is mapped to 0 and :b is mapped to 1
+
+
+;;  Once we have a mmping, we can use it on new data and transform it
+;;  into numerical values
+(def numerical-categorical-data
+  (ds-cat/transform-categorical-map
+   (ds/->dataset {:x [:a :b :a :b :b :b]})
+   x-mapping))
+numerical-categorical-data
+
+;; We can revert it as well:
+;;
+(ds-cat/invert-categorical-map numerical-categorical-data x-mapping)
+
+;;  We can as well aske about all mapping of a dataset:
+(ds-cat/dataset->categorical-maps numerical-categorical-data)
+
+
+;; **Warning:** Categorical maps attached to a column **change semantic value** of Column
+;;
+;;The existence of categorical maps on a column,
+;;change the semantic value of the data. When categorcial maps
+;;are different of two columns (for whatever reasons), it is not given
+;;that the column cell value like `0` means the same in both columns.
+;;Columns which have categorical maps should never be compared via
+;;'clojure.core/=' as this will ignore the categorical maps
+;; (unless we are sure that the categorical maps in both are **the same**)
+;; They should be converted back to their original space and then compared.
+;; This is specillay important for comparing `prediction` and `true value`
+;; in machine learning for metric calculations
+
+;;  The `dataset` namespace has as well a convienience function
+;;  in which several columns can be choose for conversion.
+(ds/categorical->number categorical-ds [:x :y])
+
+;; This works as well with filter function from namespace `column-filters`
+(require '[tech.v3.dataset.column-filters :as ds-cf])
+;; to convert all categorical columns, for example:
+(ds/categorical->number categorical-ds ds-cf/categorical)
+
+;; ### one-hot-encoding
+;;
+;;For some models / use cases the categorical data need to be converted
+;;in the so called `one-hot` format
+;;In this every column get multiplied by the number of categories , and
+;;the each one hot column can only have 0 and 1 values.
+;;
+(def one-hot-map-x (ds-cat/fit-one-hot categorical-ds :x))
+(def one-hot-map-y (ds-cat/fit-one-hot categorical-ds :y))
+one-hot-map-x
+one-hot-map-y
+
+(-> categorical-ds
+    (ds-cat/transform-one-hot one-hot-map-x)
+    (ds-cat/transform-one-hot one-hot-map-y))
+
+;;  There are similar functions to convert this format back
+;;
