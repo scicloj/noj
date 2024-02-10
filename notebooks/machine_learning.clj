710595fb7f2b2ff79f5e643395b064bffd9b8dbf
Carsten Behring <carsten.behring@gmail.com>
2024-02-10 18:18:01 +0100

    more details on "wrong comparsions of mapped columns"

--- notebooks/machine_learning.clj
+++ notebooks/machine_learning.clj
@@ -17,37 +17,38 @@
 
 ;; # Machine learning specific functionality in `tech.ml.dataset`
 ;; The library `tech.ml.dataset` contains several functions
-;; operating on a daaset, which are mainly used in teh context of machine learining
-;; In the following we will introduce those.
+;; operating on a dataset, which are mainly used in the context of
+;; machine learining. In the following we will introduce those.
 ;;
 
 ;;## Categorical variables
-;;One typicall problem in machine learining is `classification`,
+;;One typical problem in machine learning is `classification`,
 ;;so learning how to categorize data in different categories.
 ;;Sometimes data in this format is as well called "qualitative data"
-;;or data aving `discrete` values
+;;or data having `discrete` values
 
-;; These categories are often expressed in Clojure the data as of
-;; being  `type` String or `keyword`
+;; These categories are often expressed in Clojure as of
+;; being  of type `String` or `keyword`
 ;;
 ;; In `dataset` it is the `Column` which has specific support for
 ;; categorical data
 
-;; Creating a column out of categorical data like this:
+;; Creating a column out of categorical data looks like this:
 
 (def column-x (col/new-column  :x  [:a :b]))
 
-;; creates a "categorical" column, which is marked as such in
+;; This creates a "categorical" column, which is marked as such in
 ;; the column metadata.
 
-;; Printing the var show its "type" as being `keyword`
+;; Printing the var shows its "type" as being `keyword`
 column-x
-;; and printint its metadata show that it got marked as `categorical`
+;; and printing its metadata show that it got marked as `categorical`
 (meta column-x)
 
-;; The column is therefore using its metdata, and it is important to get used to look
-;; at it in case of debugging issues.
-;; The same happens, when creating a `dataset` which is at thend end a seq
+;; The column is therefore using its metadata to store important information,
+;; and it is important to get used to look at it for the case of debugging
+;; issues.
+;; The same happens, when creating a `dataset` which is a seq
 ;; of columns
 ;;
 (def categorical-ds
@@ -60,13 +61,14 @@ categorical-ds
  meta
  (vals categorical-ds))
 
-;; ### Express categorical vars in numeric space
+;; ### Express categorical variables in numeric space
 ;; Most machine learining models can only work on numerical values,
-;; both for features and the target variable
-;; So usualy we need to transform categorical data into a numeric representation,
+;; both for features and the target variable.
+;; So usually we need to transform categorical data into a numeric representation,
 ;; so each category need to be converted to a number.
-;;  These numbers have no meaning for the users, so oftne we need to convert bacl into
-;;  String / keyword space
+;;  These numbers have no meaning for the users, so often we need to
+;;  convert back into
+;;  String / keyword space later on.
 ;;
 ;; Namespace `tech.ml.dataset.classification`
 ;; has several functions to do so.
@@ -75,25 +77,23 @@ categorical-ds
 (require  '[tech.v3.dataset.categorical :as ds-cat])
 
 ;; These function operate on a single column, but expect a dataset and
-;; a colum name as input
+;; a column name as input.
 ;;
-;; We can ask it to find a mapping from string/keyword to a
+;; We use them to find a mapping from string/keyword to a
 ;; numerical space (0 ... x) like this
 
 (ds-cat/fit-categorical-map categorical-ds :x)
 
-;; This maps value in the order of occurence in the column to
+;; This maps value in the order of occurrence in the column to
 ;; 0 .. 1
 ;; This is a bit dangerous, as the mapping is decided by "row order",
 ;; which could change or be different on other subset of the data
-;; So it is prefered to be specified explicitely
+;; So it is preferred to be specified explicitly.
 
 (def x-mapping (ds-cat/fit-categorical-map categorical-ds :x [:a :b]))
 x-mapping
-;;  Now we know for sure, that :a is mapped to 0 and :b is mapped to 1
-
-
-;;  Once we have a mmping, we can use it on new data and transform it
+;;  Now we know for sure, that :a is mapped to 0 and :b is mapped to 1.
+;;  Once we have a mapping, we can use it on new data and transform it
 ;;  into numerical values
 (def numerical-categorical-data
   (ds-cat/transform-categorical-map
@@ -105,24 +105,92 @@ numerical-categorical-data
 ;;
 (ds-cat/invert-categorical-map numerical-categorical-data x-mapping)
 
-;;  We can as well aske about all mapping of a dataset:
+;;  We can as well ask about all mapping of a dataset:
 (ds-cat/dataset->categorical-maps numerical-categorical-data)
 
 
-;; **Warning:** Categorical maps attached to a column **change semantic value** of Column
+;; ## **Warning:** Categorical maps attached to a column **change semantic value** of the Column
 ;;
 ;;The existence of categorical maps on a column,
-;;change the semantic value of the data. When categorcial maps
-;;are different of two columns (for whatever reasons), it is not given
+;;change the semantic value of the data. When categorical maps
+;;are different for two columns (for whatever reasons), it is not given
 ;;that the column cell value like `0` means the same in both columns.
 ;;Columns which have categorical maps should never be compared via
-;;'clojure.core/=' as this will ignore the categorical maps
+;;'clojure.core/=' as this will ignore the categorical maps.
 ;; (unless we are sure that the categorical maps in both are **the same**)
 ;; They should be converted back to their original space and then compared.
-;; This is specillay important for comparing `prediction` and `true value`
-;; in machine learning for metric calculations
+;; This is specially important for comparing `prediction` and `true value`
+;; in machine learning for metric calculations.
 
-;;  The `dataset` namespace has as well a convienience function
+;; ### Incorrect comparisons
+;; In this ds the two columns are clearly different (the opposite even)
+(def ds-with-different-cat-maps
+  (->
+   (ds/->dataset {:x-1 [:a :b :a :b :b :b]
+                  :x-2 [:b :a :b :a :a :a]})
+   (ds/categorical->number [:x-1 :x-2])))
+
+
+(:x-1 ds-with-different-cat-maps)
+(:x-2 ds-with-different-cat-maps)
+
+;; By using default `/categorical->number` we get different categorical
+;; maps, having different :lookup-tables
+(meta (:x-1 ds-with-different-cat-maps))
+(meta (:x-2 ds-with-different-cat-maps))
+
+;;  so they are (wrongly) compared as equal
+(=
+ (:x-1 ds-with-different-cat-maps)
+ (:x-2 ds-with-different-cat-maps))
+
+;; ### Correct comparison
+;; In order to compare them correctly,
+;; we need to first revert the categorical mappings
+
+(def reverted-ds-with-different-cat-maps
+  (ds-cat/reverse-map-categorical-xforms ds-with-different-cat-maps))
+
+(:x-1 reverted-ds-with-different-cat-maps)
+(:x-2 reverted-ds-with-different-cat-maps)
+
+
+;;  and can know compare them correctly as :false
+(=
+ (:x-1 reverted-ds-with-different-cat-maps)
+ (:x-2 reverted-ds-with-different-cat-maps))
+
+;; So it should be as well avoid to transform mapped columns
+;; to other representations, which loose the mappings, like tensor
+;; or primitive arrays, or even sequences
+
+;; ### Better use same and fixed mapping
+;; This issue can be avoided by specifying the mapping to use, as being
+;; {:a 0  :b 1}
+(def ds-with-same-cat-maps
+  (->
+   (ds/->dataset {:x-1 [:a :b :a :b :b :b]
+                  :x-2 [:b :a :b :a :a :a]})
+   (ds/categorical->number [:x-1 :x-2] [:a :b])))
+
+;; mapping spec can be either [:a :b] or  [:a 0 :b 1]
+
+(:x-1 ds-with-same-cat-maps)
+(:x-2 ds-with-same-cat-maps)
+
+;;  we get same categorical maps
+(meta (:x-1 ds-with-same-cat-maps))
+(meta (:x-2 ds-with-same-cat-maps))
+
+;;  so they are correctly compared as not equal
+(=
+ (:x-1 ds-with-same-cat-maps)
+ (:x-2 ds-with-same-cat-maps))
+
+
+;; ## Convert several columns in one go
+
+;;  The `dataset` namespace has as well a convenience function
 ;;  in which several columns can be choose for conversion.
 (ds/categorical->number categorical-ds [:x :y])
 
@@ -136,16 +204,25 @@ numerical-categorical-data
 ;;For some models / use cases the categorical data need to be converted
 ;;in the so called `one-hot` format.
 ;;In this every column get multiplied by the number of categories , and
-;;the each one hot column can only have 0 and 1 values.
+;;the each one-hot column can only have 0 and 1 values.
 ;;
 (def one-hot-map-x (ds-cat/fit-one-hot categorical-ds :x))
 (def one-hot-map-y (ds-cat/fit-one-hot categorical-ds :y))
 one-hot-map-x
 one-hot-map-y
 
-(-> categorical-ds
-    (ds-cat/transform-one-hot one-hot-map-x)
-    (ds-cat/transform-one-hot one-hot-map-y))
+categorical-ds
+
+;;  get transformed by
+
+(def one-hot-ds
+  (-> categorical-ds
+      (ds-cat/transform-one-hot one-hot-map-x)
+      (ds-cat/transform-one-hot one-hot-map-y)))
+
+;; into
+
+one-hot-ds
 
 ;;  There are similar functions to convert this format back
 ;;
@@ -155,32 +232,34 @@ one-hot-map-y
 
 
 ;; A dataset for machine learning has always two groups of columns.
-;; They can eithe be the `features` or the `inference targets`.
-;; The goal of learining is to find teh relation ship between te tow groups
-;; and therefore be able to `predict` inference targets form features.
+;; They can either be the `features` or the `inference targets`.
+;; The goal of learining is to find the relation ship between
+;; the two groups
+;; and therefore be able to `predict` inference targets from features.
 ;; Sometimes the features are called `X` and the targets `y`.
 
 ;;  When constructing a dataset
 (def ds
   (ds/->dataset {:x-1 [0 1 0]
-                   :x-2 [1 0 1]
-                   :y [:a :a :b]}))
+                 :x-2 [1 0 1]
+                 :y [:a :a :b]}))
 
 ;; we need to mark explicitely which columns are `features` and which are `target`
-;; in order to be able to use it later for machine learnining in `metamorph.ml`
+;; in order to be able to use it later for machine learning in `metamorph.ml`
 ;;
-;; As normlay only 1 or a few columns are inference targets, we can simply mark those
-;; and the rest is regarded as features.
+;; As normally only one or a few columns are inference targets,
+;; we can simply mark those and the rest is regarded as features.
+
 (require  '[tech.v3.dataset.modelling :as ds-mod])
 (def modelled-ds
   (-> ds
       (ds-mod/set-inference-target :y)))     ; works as well with seq
 
-;; this is marked as well in the column metadata
+;; This is marked as well in the column metadata.
 (-> modelled-ds :y meta)
 
 
-;;  there are several functins to get information on faetures and inference targets:
+;; There are several functions to get information on features and inference targets:
 
 (ds-mod/feature-ecount modelled-ds)
 
@@ -188,10 +267,13 @@ one-hot-map-y
 
 (ds-cf/target modelled-ds)
 
-;; ## combining categorical transformation and modelling
+
+
+;; ## Combining categorical transformation and modelling
 ;;
 ;;
-;;  Very often we need to do both for doing classification and
+;;  Very often we need to do transform and model for doing
+;;  classification and
 ;;  combine the ->numeric transformation of categorical vars
 ;;  and the marking of inference target
 (def ds-ready-for-train
@@ -208,17 +290,24 @@ one-hot-map-y
 
 ds-ready-for-train
 
-;;  Such a dataset is reday for training as it
-;;  only contains numerical variables
+;;  Such a dataset is ready for training as it
+;;  only contains numerical variables (having the categorical map in place for easy converting back, if needed)
+;;  and the inference target is marked,
+;;  as we can see in the meta data:
+;;
+(map meta (vals ds-ready-for-train))
 ;;
 ;; Most models in the `metamorph.ml` ecosystem can work with
 ;; data in this format.
-;; If needed, data could as well be easely transfomred into a tensor.
-;; Most models do this internaly anyway (often to primitive arrays)
+;;
+;;
+;; Side remark:
+;; If needed, data could as well be easily transformed into a tensor.
+;; Most models do this internally anyway (often to primitive arrays)
 (def ds-tensor
   (tech.v3.dataset.tensor/dataset->tensor ds-ready-for-train))
 ds-tensor
 
-;;  or we can do so, if needed, but this looses the notition of features /
-;;  inferene target
+;;  or we can do so, if needed, but this looses the notation of features /
+;;  inference target
 (tech.v3.tensor/->jvm ds-tensor)
