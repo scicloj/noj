adaeff1fea1ae5fb63fde7df6c60c4f21aa1eb95
daslu <daniel.slutsky@gmail.com>
2024-08-21 00:27:36 +0300

    recommend lines

--- notebooks/noj_book/recommended_libraries.clj
+++ notebooks/noj_book/recommended_libraries.clj
@@ -5,8 +5,11 @@
 
 ;; We recommend combining Noj with the following libraries:
 ;; * [Wolframite](https://scicloj.github.io/wolframite/) - Wolfram Language bindings
+;; * [kixi.stats](https://github.com/MastodonC/kixi.stats) - statistics with transducers
+;; * [fastmath-clustering](https://generateme.github.io/fastmath-clustering/) - a wrapper for [Smile](https://haifengl.github.io/) 2 clustering algorithms
+;; * [scicloj.ml.smile](https://github.com/scicloj/scicloj.ml.smile) - Smile 2 models for [metamorph.ml](https://github.com/scicloj/metamorph.ml)
 ;; * [clj-djl](https://github.com/scicloj/clj-djl) - bindings for [Deep Java Library](https://djl.ai/)
-;; * [scicloj.ml.clj-djl](https://github.com/scicloj/scicloj.ml.clj-djl) - clj-djl models for the metamorph.ml plaform
+;; * [scicloj.ml.clj-djl](https://github.com/scicloj/scicloj.ml.clj-djl) - clj-djl models for [metamorph.ml](https://github.com/scicloj/metamorph.ml)
 ;; * [Neanderthal](https://neanderthal.uncomplicate.org/) - high-performance numerical computing
 ;; * [deep-diamond](https://github.com/uncomplicate/deep-diamond) - tensors & deep learning
 ;; * [llama.clj](https://github.com/phronmophobic/llama.clj) - running LLMs locally
