70a851c760fcdff5d11d62747a994fe1190ef70b
daslu <daniel.slutsky@gmail.com>
2024-08-20 23:52:21 +0300

    added recommended libraries page

---
+++ notebooks/noj_book/recommended_libraries.clj
@@ -0,0 +1,8 @@
+;; # Recommended libraries
+
+^:kindly/hide-code
+(ns noj-book.recommended-libraries)
+
+;; We recommend combining Noj with the following libraries:
+
+;; * [Wolframite](https://scicloj.github.io/wolframite/) - Wolfram Language bindings
