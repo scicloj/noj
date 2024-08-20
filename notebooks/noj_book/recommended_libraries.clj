205712a1c0b588e7af6ea695a11c7d49c51b1411
daslu <daniel.slutsky@gmail.com>
2024-08-21 00:39:03 +0300

    recommended_libraries

--- notebooks/noj_book/recommended_libraries.clj
+++ notebooks/noj_book/recommended_libraries.clj
@@ -4,14 +4,15 @@
 (ns noj-book.recommended-libraries)
 
 ;; We recommend combining Noj with the following libraries:
+
 ;; * [Wolframite](https://scicloj.github.io/wolframite/) - Wolfram Language bindings
 ;; * [cljplot](https://github.com/generateme/cljplot) - plotting - 🛠 early stage
 ;; * [kixi.stats](https://github.com/MastodonC/kixi.stats) - statistics with transducers
 ;; * [fitdistr](https://github.com/generateme/fitdistr) - fitting distributions
 ;; * [inferme](https://github.com/generateme/inferme) - Bayesian inference
 ;; * [cmdstan-clj](https://github.com/scicloj/cmdstan-clj) - bindings for [Stan](https://mc-stan.org/) Bayesian inference - 🛠 early stage
-;; * [fastmath-clustering](https://generateme.github.io/fastmath-clustering/) - a wrapper for [Smile](https://haifengl.github.io/) 2 clustering algorithms
-;; * [scicloj.ml.smile](https://github.com/scicloj/scicloj.ml.smile) - Smile 2 models for [metamorph.ml](https://github.com/scicloj/metamorph.ml)
+;; * [fastmath-clustering](https://generateme.github.io/fastmath-clustering/) - a wrapper for [Smile](https://haifengl.github.io/) 2.6 clustering algorithms
+;; * [scicloj.ml.smile](https://github.com/scicloj/scicloj.ml.smile) - [Smile](https://haifengl.github.io/) 2.y models for [metamorph.ml](https://github.com/scicloj/metamorph.ml)
 ;; * [clj-djl](https://github.com/scicloj/clj-djl) - bindings for [Deep Java Library](https://djl.ai/)
 ;; * [scicloj.ml.clj-djl](https://github.com/scicloj/scicloj.ml.clj-djl) - clj-djl models for [metamorph.ml](https://github.com/scicloj/metamorph.ml)
 ;; * [Neanderthal](https://neanderthal.uncomplicate.org/) - high-performance numerical computing
