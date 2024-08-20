da644fba8e6bd825d778b81a9a07df1ad38bec30
daslu <daniel.slutsky@gmail.com>
2024-08-21 00:24:01 +0300

    recommended libraries

--- notebooks/noj_book/recommended_libraries.clj
+++ notebooks/noj_book/recommended_libraries.clj
@@ -4,5 +4,10 @@
 (ns noj-book.recommended-libraries)
 
 ;; We recommend combining Noj with the following libraries:
-
 ;; * [Wolframite](https://scicloj.github.io/wolframite/) - Wolfram Language bindings
+;; * [clj-djl](https://github.com/scicloj/clj-djl) - bindings for [Deep Java Library](https://djl.ai/)
+;; * [scicloj.ml.clj-djl](https://github.com/scicloj/scicloj.ml.clj-djl) - clj-djl models for the metamorph.ml plaform
+;; * [Neanderthal](https://neanderthal.uncomplicate.org/) - high-performance numerical computing
+;; * [deep-diamond](https://github.com/uncomplicate/deep-diamond) - tensors & deep learning
+;; * [llama.clj](https://github.com/phronmophobic/llama.clj) - running LLMs locally
+;; * [Bosquet](https://github.com/zmedelis/bosquet) - tooling for LLM applications
