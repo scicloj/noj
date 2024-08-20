9e09c501e82254c33c82f86a229d67b2fbcbc9ce
daslu <daniel.slutsky@gmail.com>
2024-08-21 00:32:00 +0300

    recommended libraries

--- notebooks/noj_book/recommended_libraries.clj
+++ notebooks/noj_book/recommended_libraries.clj
@@ -5,12 +5,17 @@
 
 ;; We recommend combining Noj with the following libraries:
 ;; * [Wolframite](https://scicloj.github.io/wolframite/) - Wolfram Language bindings
+;; * [cljplot](https://github.com/generateme/cljplot) - plotting - 🛠 early stage
 ;; * [kixi.stats](https://github.com/MastodonC/kixi.stats) - statistics with transducers
+;; * [fitdistr](https://github.com/generateme/fitdistr) - fitting distributions
+;; * [inferme](https://github.com/generateme/inferme) - Bayesian inference
+;; * [cmdstan-clj](https://github.com/scicloj/cmdstan-clj) - bindings for [Stan](https://mc-stan.org/) Bayesian inference - 🛠 early stage
 ;; * [fastmath-clustering](https://generateme.github.io/fastmath-clustering/) - a wrapper for [Smile](https://haifengl.github.io/) 2 clustering algorithms
 ;; * [scicloj.ml.smile](https://github.com/scicloj/scicloj.ml.smile) - Smile 2 models for [metamorph.ml](https://github.com/scicloj/metamorph.ml)
 ;; * [clj-djl](https://github.com/scicloj/clj-djl) - bindings for [Deep Java Library](https://djl.ai/)
 ;; * [scicloj.ml.clj-djl](https://github.com/scicloj/scicloj.ml.clj-djl) - clj-djl models for [metamorph.ml](https://github.com/scicloj/metamorph.ml)
 ;; * [Neanderthal](https://neanderthal.uncomplicate.org/) - high-performance numerical computing
+;; * [matlib](https://github.com/atisharma/matlib) - optimisation and control theory with Neanderthal
 ;; * [deep-diamond](https://github.com/uncomplicate/deep-diamond) - tensors & deep learning
 ;; * [llama.clj](https://github.com/phronmophobic/llama.clj) - running LLMs locally
 ;; * [Bosquet](https://github.com/zmedelis/bosquet) - tooling for LLM applications
