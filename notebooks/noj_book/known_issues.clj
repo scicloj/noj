84b7ad9bdf521da76211485f8b27df072daaf6c3
daslu <daniel.slutsky@gmail.com>
2024-08-23 00:58:21 +0300

    doc title correction

--- notebooks/noj_book/known_issues.clj
+++ notebooks/noj_book/known_issues.clj
@@ -1,11 +1,11 @@
-;; ## Known issues ❗
+;; # Known issues ❗
 
 ^:kindly/hide-code
 (ns noj-book.known-issues
   (:require [scicloj.kindly.v4.kind :as kind]
             [scicloj.kindly.v4.api :as kindly]))
 
-;; ### scicloj.ml.tribuo
+;; ## scicloj.ml.tribuo
 
 ;; Due to a current bug regarding cyclic depdendencies, when using [scicloj.ml.tribuo](https://github.com/scicloj/scicloj.ml.tribuo) for machine learning, it is necessary to include it explicitly in your project dependencies (even though it is a depedency of Noj itself):
 
