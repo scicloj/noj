0c7566d6ebad3767b20a7e2cd2e493d321e3e977
daslu <daniel.slutsky@gmail.com>
2024-09-27 23:54:36 +0300

    updated underlying libraries page

--- notebooks/noj_book/underlying_libraries.clj
+++ notebooks/noj_book/underlying_libraries.clj
@@ -11,10 +11,10 @@
 ;; * [tmd-parquet](https://github.com/techascent/tech.parquet) - TMD bindings for [Parquet](https://parquet.apache.org/) format
 ;; * [dtype-next](https://github.com/cnuernber/dtype-next) - high-performance array-programming
 ;; * [Kindly](https://scicloj.github.io/kindly-noted/) - datavis standard
-;; * [Fastmath](https://github.com/generateme/fastmath) - math & stats - 🛠 [snapshot](https://clojars.org/generateme/fastmath/versions/3.0.0-SNAPSHOT) of the upcoming version 3
+;; * [Fastmath](https://github.com/generateme/fastmath) - math & stats - alpha stage of version 3
 ;; * [Hanamicloth](https://scicloj.github.io/hanamicloth/) - easy layered graphics - 🛠 alpha version - should stabilize soon
 ;; * [Hanami](https://github.com/jsa-aerial/hanami) - interactive datavis
-;; * [metamorph.ml](https://github.com/scicloj/metamorph.ml) - machine learning platform - 🛠 using the [addLinearRegression branch](https://github.com/scicloj/metamorph.ml/tree/addLinearRegression) with upcoming regression functions
+;; * [metamorph.ml](https://github.com/scicloj/metamorph.ml) - machine learning platform
 ;; * [scicloj.ml.tribuo](https://github.com/scicloj/scicloj.ml.tribuo) - [Tribuo](https://tribuo.org/) machine learning models - see [known issues](./noj_book.known_issues.html) ❗
 ;; * some Tribuo modules added by default: general-linear and tree ensembles for regression/classification
 ;; * [libpython-clj](https://github.com/clj-python/libpython-clj) - Python bindings
