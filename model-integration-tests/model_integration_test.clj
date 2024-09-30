1f414427686114f51ead8d4d012c4623518fc602
Carsten Behring <carsten.behring@gmail.com>
2024-09-30 20:08:35 +0000

    added tests for all models and shapes of target column

--- model-integration-tests/model_integration_test.clj
+++ model-integration-tests/model_integration_test.clj
@@ -8,10 +8,21 @@
             [clojure.string :as str]
             [clojure.set :as set]
             [clojure.test :refer [is deftest]]
-            [tech.v3.dataset :as ds])
-  (:import 
-   (smile.base.mlp ActivationFunction Cost HiddenLayerBuilder LayerBuilder OutputFunction OutputLayerBuilder))
-  )
+            [tech.v3.dataset :as ds]
+            [taoensso.nippy :as nippy]
+            [libpython-clj2.python :as py])
+  (:import
+   [java.util.logging Logger]
+   [org.slf4j.bridge SLF4JBridgeHandler]
+   (smile.base.mlp ActivationFunction Cost HiddenLayerBuilder LayerBuilder OutputFunction OutputLayerBuilder)))
+
+(py/initialize!)
+(py/run-simple-string "
+import warnings
+warnings.simplefilter('ignore')")
+(SLF4JBridgeHandler/removeHandlersForRootLogger) 
+(SLF4JBridgeHandler/install)
+
 
 (def mlp-hidden-layer-builder
   (HiddenLayerBuilder. 1 (ActivationFunction/linear)))
@@ -24,81 +35,103 @@
          '[scicloj.ml.smile.classification]
          '[scicloj.ml.tribuo]
          '[scicloj.sklearn-clj.ml]
-         '[scicloj.ml.xgboost]
-         )
+         '[scicloj.ml.xgboost])
+
+(def not-writable--or-readable-with-nippy
+  
+  #{:smile.classification/mlp  ;;https://github.com/scicloj/scicloj.ml.smile/issues/18
+    :scicloj.ml.tribuo/classification ;;https://github.com/scicloj/scicloj.ml.tribuo/issues/5
+    })
+
+(def not-working-with-iris-data-or-default-params-or-no-probab
+  #{:smile.classification/sparse-svm
+    :smile.classification/maxent-binomial
+    :smile.classification/maxent-multinomial
+    :smile.classification/mlp
+    :smile.classification/svm
+    :smile.classification/sparse-logistic-regression
+    :smile.classification/discrete-naive-bayes
+    :smile.classification/knn ;; check
+
+    ;;https://github.com/scicloj/sklearn-clj/issues/13
+    :sklearn.classification/ridge-classifier-cv
+    :sklearn.classification/linear-svc
+    :sklearn.classification/passive-aggressive-classifier
+    :sklearn.classification/nearest-centroid
+    :sklearn.classification/ridge-classifier
+    :sklearn.classification/bernoulli-nb
+    :sklearn.classification/perceptron
+    :sklearn.classification/sgd-classifier
+    :sklearn.classification/svc
+    :sklearn.classification/nu-svc})
 
 (def min-accuracies
-  {:smile.classification/linear-discriminant-analysis 0.85})
+  {:smile.classification/linear-discriminant-analysis 0.82
+   :smile.classification/gradient-tree-boost 0.92
+
+   :sklearn.classification/complement-nb 0.65
+   :sklearn.classification/ridge-classifier 0.30
+   :sklearn.classification/extra-tree-classifier 0.91
+   :sklearn.classification/dummy-classifier 0.2
+
+   :metamorph.ml/dummy-classifier 0.2})
 
 (def smile-model-specs
   (map
-   #(vector (get min-accuracies % 0.95)
+   #(vector (get min-accuracies % 0.93)
             {:model-type %})
    (->> (ml/model-definition-names)
         (filter #(str/starts-with? (namespace %) "smile.classification"))
         set
         ((fn [x] (set/difference
                   x
-                  #{:smile.classification/sparse-svm
-                    :smile.classification/maxent-binomial
-                    :smile.classification/maxent-multinomial
-                    :smile.classification/mlp
-                    :smile.classification/svm
-                    :smile.classification/sparse-logistic-regression
-                    :smile.classification/discrete-naive-bayes}))))))
+                  not-working-with-iris-data-or-default-params-or-no-probab))))))
 
 
 
 (def sklearn-model-specs
-  (map 
-   #(vector 0.90 
+  (map
+   #(vector (get min-accuracies % 0.90)
             {:model-type %})
    (->> (ml/model-definition-names)
-        (filter #(str/starts-with? (namespace %) "sklearn.classification" ))
-         set
-         ((fn [x] (set/difference
-                   x
-                   #{:sklearn.classification/perceptron
-                     :sklearn.classification/sgd-classifier
-                     :sklearn.classification/svc
-                     
-                     })))
-   )))
+        (filter #(str/starts-with? (namespace %) "sklearn.classification"))
+        set
+        ((fn [x] (set/difference
+                  x
+                  not-working-with-iris-data-or-default-params-or-no-probab))))))
 
-
-(def model-specs 
-  (concat
-   [
-    [0.98 {
-          ;;  :validate-parameters 1
-          ;;  :round 10
-          ;;  :silent 0
-          ;;  :verbosity 3
-           :model-type :xgboost/classification}]
-    [0.30 {:model-type :smile.classification/mlp
-           :layer-builders [mlp-hidden-layer-builder mlp-output-layer-builder]}]
-    [0.95 {:model-type :sklearn.classification/decision-tree-classifier}]
-    [0.95 {:model-type :sklearn.classification/random-forest-classifier}]
-    [0.95 {:model-type :sklearn.classification/logistic-regression}]
+(def tribuo-model-specs [
     [0.93 {:model-type :scicloj.ml.tribuo/classification
-           :tribuo-components [{:name "logistic"
-                                :type "org.tribuo.classification.sgd.linear.LinearSGDTrainer"
-                                :properties {:seed "1234"
-                                             :shuffle "false" 
-                                             :epochs "10"}}]
-           :tribuo-trainer-name "logistic"}]
-    [0.94 {:model-type :scicloj.ml.tribuo/classification
-           :tribuo-components [{:name "random-forest"
-                                :type "org.tribuo.classification.dtree.CARTClassificationTrainer"
-                                :properties {:maxDepth "8"
-                                             :useRandomSplitPoints "false"
-                                             :fractionFeaturesInSplit "0.5"}}]
-           :tribuo-trainer-name "random-forest"}]
-    [0.30 {:model-type :metamorph.ml/dummy-classifier}]     
-    ]
+       :tribuo-components [{:name "logistic"
+                            :type "org.tribuo.classification.sgd.linear.LinearSGDTrainer"
+                            :properties {:seed "1234"
+                                         :shuffle "false"
+                                         :epochs "10"}}]
+       :tribuo-trainer-name "logistic"}]
+[0.93 {:model-type :scicloj.ml.tribuo/classification
+       :tribuo-components [{:name "random-forest"
+                            :type "org.tribuo.classification.dtree.CARTClassificationTrainer"
+                            :properties {:maxDepth "8"
+                                         :useRandomSplitPoints "false"
+                                         :fractionFeaturesInSplit "0.5"}}]
+       :tribuo-trainer-name "random-forest"}]
+                   
+])
+(def xgboost-specs [
+                    [0.94 {:model-type :xgboost/classification}]])
+
+(def other-specs [
+                  [0.28 {:model-type :smile.classification/mlp
+                         :layer-builders [mlp-hidden-layer-builder mlp-output-layer-builder]}]
+                  [0.30 {:model-type :metamorph.ml/dummy-classifier}]])
+
+(def model-specs
+  (concat
+   xgboost-specs
+   other-specs
+   tribuo-model-specs
    smile-model-specs
-   ;sklearn-model-specs
-   ))
+   sklearn-model-specs))
 
 
 
@@ -107,14 +140,40 @@
 (defn my-classification-accuracy [lhs rhs]
   ;(println :lhs (meta lhs))
   ;(println :rhs (meta rhs))
-  
-  (loss/classification-accuracy lhs rhs)
-  )
 
-(defn verify-classification [model-spec expected-accuracy ds]
+  (loss/classification-accuracy lhs rhs))
+
+(defn- validate-nippy-round-trip [model-spec result val-ds]
+  (let [tmp-file (java.io.File/createTempFile
+                  (format "model-%s-" (:model-type model-spec))
+                  ".nippy")
+
+        _ (nippy/freeze-to-file
+           tmp-file
+           (->> result first first :fit-ctx :model))
+        new-model (nippy/thaw-from-file tmp-file)
+
+        new-prediction
+        (-> val-ds
+         (ml/predict new-model)
+         (ds-cat/reverse-map-categorical-xforms)
+         :species)
+        new-trueth
+        (-> val-ds
+         ds-cat/reverse-map-categorical-xforms
+         :species)
+
+        new-accurcay
+        (loss/classification-accuracy
+         new-prediction
+         new-trueth)]
+    (is (<
+         (get min-accuracies (:model-type model-spec) 0.7)
+         new-accurcay))))
+
+(defn classify [model-spec ds]
   (println :verify (:model-type model-spec))
-  (let [ 
-        train-test-split
+  (let [train-test-split
         (tc/split->seq ds :kfold {:seed 1234 :k 10})
 
         pipe
@@ -128,42 +187,94 @@
          train-test-split
          my-classification-accuracy
          :accuracy)
-        accuracy (-> result first first :train-transform :mean)
-        ]
-
-    (is (>= accuracy expected-accuracy)
-            (format "%s: expect at least: %s, found : %s" 
-                    (:model-type model-spec)
-                    expected-accuracy accuracy))))
+        _
+        (when (not (contains? not-writable--or-readable-with-nippy
+                              (:model-type model-spec)))
+          (validate-nippy-round-trip model-spec result ds))
 
 
-(deftest verify-classifictions-iris
-  (run!
-   (fn [[acc spec]] (verify-classification spec acc (data/iris-ds)))
+
+        accuracy (-> result first first :test-transform :mean)]
+
+
+    accuracy))
+
+(defn- remove-model-type [model-specs type]
+  (remove
+   #(= type
+       (-> % second :model-type))
    model-specs))
 
-
-(def iris-2
-  (->
-   (data/iris-ds)
-   ds-cat/reverse-map-categorical-xforms
-   ))
-
-;; (deftest verify-classification-iris-2
-;;   (run!
-;;    (fn [[acc spec]] (verify-classification spec acc iris-2))
-;;    smile-model-specs))
-
-
-(def iris-3
-  (->
-   (data/iris-ds)
-   (ds/assoc-metadata [:species] :categorical-map nil)
-   ))
+(defn- verify-fn [[expected-acc spec] iris]
+  (try
+    (let [acc (classify spec iris)]
+      (is
+       (>= acc
+           expected-acc)
+  
+       (format "%s: expect at least: %s, found : %s"
+               (:model-type spec)
+               expected-acc acc)))
+  
+    (catch Exception e  (is false e)))
+  )
 
 
-(deftest verify-classification-iris-3
-  (run!
-   (fn [[acc spec]] (verify-classification spec acc iris-3))
-   smile-model-specs))
+
+
+(deftest verify-classification-iris-int-catmap
+  (let [iris  (data/iris-ds)]
+    (run!
+     #(verify-fn % iris)
+     model-specs)))
+
+(deftest verify-classification-iris-float-catmap
+  (let [iris
+        (->
+         (data/iris-ds)
+         ds-cat/reverse-map-categorical-xforms
+         ( ds/categorical->number [:species] {} :float-64))]
+    (run!
+     #(verify-fn % iris)
+     model-specs)))
+
+ (deftest verify-classification-iris-no-catmap
+  (let [iris
+        (->
+         (data/iris-ds)
+         ds-cat/reverse-map-categorical-xforms 
+         )]
+    (run!
+     #(verify-fn % iris)
+     ;; only tribuo can deal with "string" target
+     ;;https://github.com/scicloj/noj/issues/36
+     (concat
+      ;other-specs
+      ;xgboost-specs
+      tribuo-model-specs
+      ;smile-model-specs
+      ;sklearn-model-specs
+      ))
+    ))
+
+(deftest verify-classification-iris-nil-catmap
+  (let [iris
+        (->
+         (data/iris-ds)
+         (ds/assoc-metadata [:species] :categorical-map nil))]
+    (run!
+     #(verify-fn % iris)
+      
+     (-> model-specs
+         ;;https://github.com/scicloj/scicloj.ml.tribuo/issues/6 
+         (remove-model-type  :scicloj.ml.tribuo/classification)
+         ;;https://github.com/scicloj/scicloj.ml.smile/issues/19
+         (remove-model-type  :smile.classification/mlp)
+         ;;https://github.com/scicloj/scicloj.ml.xgboost/issues/1
+         (remove-model-type  :xgboost/classification)
+         ))
+     )
+    )
+
+  ;;(classify (-> smile-model-specs first second) iris-3)
 
