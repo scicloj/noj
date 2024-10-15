a8e3cb634908e77f051d3d4c4ccbd6d9e5caf03d
daslu <daniel.slutsky@gmail.com>
2024-10-15 22:31:32 +0300

    undelying libraries - typo

--- notebooks/noj_book/underlying_libraries.clj
+++ notebooks/noj_book/underlying_libraries.clj
@@ -17,8 +17,7 @@
 ;; * [metamorph.ml](https://github.com/scicloj/metamorph.ml) - machine learning platform
 ;; * [scicloj.ml.tribuo](https://github.com/scicloj/scicloj.ml.tribuo) - [Tribuo](https://tribuo.org/) machine learning models
 ;; * [scicloj.ml.smile](https://github.com/scicloj/scicloj.ml.smile) - [Smile](https://haifengl.github.io/) (v 2.6) machine learning models
-;; * [sklearn-clj](https://github.com/scicloj/sklearn-clj) -
-;; * some Tribuo modules added by default: general-linear and tree ensembles for regression/classification
+;; * [sklearn-clj](https://github.com/scicloj/sklearn-clj) - Plugin to use [sklearn](https://scikit-learn.org/) models in metamorph.ml
 ;; * [libpython-clj](https://github.com/clj-python/libpython-clj) - Python bindings
 ;; * [kind-pyplot](https://scicloj.github.io/kind-pyplot/) - Python plotting
 ;; * [ClojisR](https://scicloj.github.io/clojisr/) - R bindings
