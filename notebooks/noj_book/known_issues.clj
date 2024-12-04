1b6f380ff923a3f37ceb35d7e611eb8ba415ab86
daslu <daniel.slutsky@gmail.com>
2024-12-04 22:25:25 +0200

    added a comment about the deps write lock issue

--- notebooks/noj_book/known_issues.clj
+++ notebooks/noj_book/known_issues.clj
@@ -5,4 +5,16 @@
   (:require [scicloj.kindly.v4.kind :as kind]
             [scicloj.kindly.v4.api :as kindly]))
 
-;; (currently none)
+;; ## Fetching dependencies
+
+;; If you encounter:
+;; _Error building classpath. Could not acquire write lock for 'artifact:org.bytedeco:mkl'_
+
+;; Try:
+;; ```bash
+;; clj -P -Sthreads 1
+;; ```
+;; from your terminal.
+
+;; See [deps issue report](https://clojurians-log.clojureverse.org/tools-deps/2021-09-16).
+
