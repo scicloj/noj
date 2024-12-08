6e8164e96cd4250c98f063b64fbc16e4866c97db
daslu <daniel.slutsky@gmail.com>
2024-12-08 18:04:46 +0200

    linear-regression-intro - WIP

--- notebooks/noj_book/linear_regression_intro.clj
+++ notebooks/noj_book/linear_regression_intro.clj
@@ -1,10 +1,21 @@
+;; # Intro to Linear Regression - DRAFT 🛠
+
+;; Here we offer an intro to [linear regression](https://en.wikipedia.org/wiki/Linear_regression)
+;; following the
+;; [In Depth: Linear Regression](https://jakevdp.github.io/PythonDataScienceHandbook/05.06-linear-regression.htmlhttps://jakevdp.github.io/PythonDataScienceHandbook/05.06-linear-regression.html)
+;; section of the
+;; [Python Data Science Handbook](https://jakevdp.github.io/PythonDataScienceHandbook/)
+;; by Jake VanderPlas.
+
+;; ## Setup
+
 (ns noj-book.linear-regression-intro
   (:require
    [tablecloth.api :as tc]
    [tablecloth.column.api :as tcc]
    [tech.v3.datatype.datetime :as datetime]))
 
-datetime/long-temporal-field
+;; ## Reading and parsing data
 
 (def column-name-mapping
   {"Fremont Bridge Sidewalks, south of N 34th St" :total
@@ -12,47 +23,29 @@ datetime/long-temporal-field
    "Fremont Bridge Sidewalks, south of N 34th St Cyclist East Sidewalk" :east
    "Date" :datetime})
 
-#_(column-name-mapping "Fremont Bridge Sidewalks, south of N 34th St")
+(column-name-mapping
+ "Fremont Bridge Sidewalks, south of N 34th St")
 
 (def counts
-  (tc/dataset "data/seattle-bikes-and-weather/Fremont_Bridge_Bicycle_Counter.csv"
+  (tc/dataset "data/seattle-bikes-and-weather/Fremont_Bridge_Bicycle_Counter.csv.gz"
               {:key-fn column-name-mapping
                :parser-fn {"Date" [:local-date-time "MM/dd/yyyy hh:mm:ss a"]}}))
 
 (def weather
-  (tc/dataset "data/seattle-bikes-and-weather/BicycleWeather.csv"
+  (tc/dataset "data/seattle-bikes-and-weather/BicycleWeather.csv.gz"
               {:key-fn keyword}))
 
+;; ## Preprocessing
+
 ;; no good support for this in tablecloth
+;; ```python
 ;; daily = counts.resample('d').sum()
+;; ```
 
 ;; day column, group by, aggregate, sum.
-
-(-> counts
-    (tc/group-by (fn [{:keys [datetime]}]
-                   (datetime/local-date-time->local-date datetime)))
-    (tc/aggregate #(tcc/sum (% :total))))
-
-(-> counts
-    (tc/group-by (fn [{:keys [datetime]}]
-                   (datetime/local-date-time->local-date datetime)))
-    (tc/aggregate (comp tcc/sum :total)))
 
 (-> counts
     (tc/group-by (fn [{:keys [datetime]}]
                    {:date (datetime/local-date-time->local-date datetime)}))
     (tc/aggregate {:total (comp tcc/sum :total)}))
 
-
-(comment
-  ;; Example from tablecloth README
-  (-> "https://raw.githubusercontent.com/techascent/tech.ml.dataset/master/test/data/stocks.csv"
-      (tc/dataset {:key-fn keyword})
-      (tc/group-by (fn [row]
-                     {:symbol (:symbol row)
-                      :year (tech.v3.datatype.datetime/long-temporal-field :years (:date row))}))
-      (tc/aggregate #(tcc/sum (% :price)))
-      (tc/order-by [:symbol :year])
-      (tc/head 10)))
-
-datetime/local-date-time->local-date
