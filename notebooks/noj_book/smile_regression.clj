354127be4314b2e76f2ab12b222a1016a22eff8b
Carsten Behring <carsten.behring@gmail.com>
2024-12-22 19:53:49 +0000

    updated to libpython-clj 2.0.26

--- notebooks/noj_book/smile_regression.clj
+++ notebooks/noj_book/smile_regression.clj
@@ -1,19 +1,13 @@
 ;; # Smile regression models reference - DRAFT 🛠
 
+^:kindly/hide-code
+(require '[scicloj.ml.smile.regression]
+         '[scicloj.ml.tribuo])
+
 (ns noj-book.smile-regression 
   (:require
    [noj-book.utils.render-tools :refer [render-key-info]]
-   [scicloj.kindly.v4.kind :as kind]
-   [scicloj.metamorph.core :as mm]
-   [scicloj.metamorph.ml :as ml]
-   [scicloj.metamorph.ml.toydata :as datasets]
-   [tablecloth.api :as tc]
-   [tech.v3.dataset :as ds]
-   [tech.v3.dataset.metamorph :as ds-mm]
-   [tech.v3.datatype.functional :as dtf]))
-
-^:kindly/hide-code
-(require '[scicloj.ml.smile.regression])
+   [scicloj.ml.tribuo]))
 
 
 ^:kindly/hide-code
