5e17d9d41b15b20cf00d7ed9e119115ebf4a2fa1
daslu <daniel.slutsky@gmail.com>
2025-10-28 21:33:51 +0200

    typo in debug code

--- build/gen_tests.clj
+++ build/gen_tests.clj
@@ -8,7 +8,7 @@
   (->>
    (file-seq (io/file "notebooks/noj_book"))
    (filter #(str/ends-with? (.toPath %) ".clj"))
-   (filter (#{"notebooks/noj_book/smile_others.clj"}
+   (filter #(#{"notebooks/noj_book/smile_others.clj"}
             (str %)))
    ((fn [paths]
       (prn [:gen-test-paths paths])
