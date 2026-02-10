5190339efa11efd8747cec61c8f94949bd925d19
Carsten Behring <carsten.behring@gmail.com>
2026-02-10 21:50:23 +0000

    added new formats to docu

--- notebooks/noj_book/datasets.clj
+++ notebooks/noj_book/datasets.clj
@@ -1,4 +1,4 @@
-;; # Example Datasets
+;; # Reading Datasets
 
 ;; author: Daniel Slutsky, Ken Huang
 
@@ -6,10 +6,66 @@
 
 (ns noj-book.datasets
   (:require [tablecloth.api :as tc]
-            [scicloj.metamorph.ml.rdatasets :as rdatasets]))
+            [scicloj.metamorph.ml.rdatasets :as rdatasets]
+            [scicloj.kindly.v4.kind :as kind]
+            ))
 
-;; ## rdatasets
-;; One of the main sources is the `rdatasets` namespace of [metamorph.ml](https://github.com/scicloj/metamorph.ml),
+;; ## Reading data from files
+;; Noj provides the correct setup, so that tablecloth can be used to read files in these
+;; formats:
+;;
+;;  * CSV and TSV
+;;  * Nippy
+;;  * Arrow
+;;  * Excel (xlsx) 
+;;  * Parquet
+;;
+;; Mostly the file type gets detected from the filename suffix
+;; (.csv, .tsv., .nippy, .xlsx, .parquet)
+
+;; ### CVS and TSV
+;; These support 
+;; * direct read from local disk
+;; * as gz compressed file
+;; * over http (as well gz compressed)
+;; * wrapped in input-stream
+
+;; See below for examples.
+
+;; ### Arrow
+;; For arrow suffix detection is not possible, as there is no standard suffix,
+;; so it must be given via option {:file-type arrow}
+;;
+;; See further documentation [here](https://techascent.github.io/tech.ml.dataset/tech.v3.libs.arrow.html#var-stream-.3Edataset).
+(tc/dataset "data/alldtypes.arrow-feather" {:file-type :arrow})
+
+
+;; ### Excel 
+;; Read single sheet Excel file:
+(comment
+ (tc/dataset "data/singleSheet.xlsx"))
+
+;;see documentation [here](https://techascent.github.io/tech.ml.dataset/tech.v3.libs.fastexcel.html).
+;;
+;; An Excel file can have various sheets, which can be accessed as a sequence
+;; of datasets:
+(comment
+ (tech.v3.libs.fastexcel/workbook->datasets "data/twoSheets.xlsx"))
+;;
+
+;;see documentation [here](https://techascent.github.io/tech.ml.dataset/tech.v3.libs.fastexcel.html#var-workbook-.3Edatasets).
+
+
+;; ### Parquet
+;; Can be read by:
+(comment (tc/dataset "data/userdata1.parquet"))
+
+;; see documentation [here](https://techascent.github.io/tech.ml.dataset/tech.v3.libs.parquet.html#var-parquet-.3Eds).
+
+
+
+;; ## Example datasets from rdatasets
+;; One of the main collections of example dataset is the `rdatasets` namespace of [metamorph.ml](https://github.com/scicloj/metamorph.ml),
 ;; which can fetch datasets from the [Rdatasets](https://vincentarelbundock.github.io/Rdatasets/) collection.
 
 (rdatasets/datasets-iris)
@@ -18,7 +74,7 @@
 
 (rdatasets/openintro-simulated_scatter)
 
-;; ## Plotly
+;; ## Example datasets from Plotly
 ;; We can also use datasets from [Plotly Sample Datasets](https://plotly.github.io/datasets/)
 
 (tc/dataset
@@ -27,7 +83,7 @@
   :parser-fn {:OPENDATE :string
               :date_super :string}})
 
-;; ## tech.ml.dataset (TMD)
+;; ## Example datasets from from tech.ml.dataset (TMD)
 ;; [TMD's repo](https://github.com/techascent/tech.ml.dataset/tree/master/test/data)
 ;; also has some datasets that we can use:
 
