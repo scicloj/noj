3b7b945315b8c1f834a2533ffb1f45def7e92aae
Cvetomir M. Dimov <cvetomir.dimov@unige.ch>
2026-09-04 11:22:07 +0200

    Modularized card code

--- pages/index.clj
+++ pages/index.clj
@@ -19,22 +19,28 @@
   [:h1 [:i "The reliable data analysis stack in Clojure"]]])
 
 ^:kindly/hide-code
-(kind/hiccup
-   [:div {:class ["d-flex" "flex-column" "flex-md-row" "gap-3"]}
-    [:div {:class ["flex-fill" "w-100" "border" "rounded" "overflow-hidden" "p-3"]}
-     [:h3 "Simple"]
-     [:p "Think clearly about the problem by disentangling concerns, Clojure-style."]
-     [:img {:src "Simple.jpeg"
-            :style {:width "100%" :height "auto" :display "block"}}]]
-    [:div {:class ["flex-fill" "w-100" "border" "rounded" "overflow-hidden" "p-3"]}
-     [:h3 "Robust"]
-     [:p "Rely on performant and battle-tested Java libraries."]
-     [:img {:src "Java.png"
-            :style {:width "100%" :height "auto" :display "block"}}]]
-    [:div {:class ["flex-fill" "w-100" "border" "rounded" "overflow-hidden" "p-3"]}
-     [:h3 "Versatile"]
-     [:p "Seamlessly transition from development to deployment."]
-     [:img {:src "Versatile.jpeg"
-            :style {:width "100%" :height "auto" :display "block"}}]]])
+(def card-data
+  [{:heading "Simple"
+    :text "Think clearly about the problem by disentangling concerns, Clojure-style."
+    :image "Simple.jpeg"}
+   {:heading "Robust"
+    :text "Rely on performant and battle-tested Java libraries."
+    :image "Java.png"}
+   {:heading "Versatile"
+    :text "Seamlessly transition from development to deployment."
+    :image "Versatile.jpeg"}])
 
+^:kindly/hide-code
+(defn feature-card [{:keys [heading text image]}]
+  [:div {:class ["flex-fill" "w-100" "border" "rounded" "overflow-hidden" "p-2"]}
+     [:h3 heading]
+     [:p text]
+     [:img {:src image
+            :style {:width "100%" :height "auto" :display "block"}}]])
+        
+^:kindly/hide-code
+(kind/hiccup
+ (into [:div {:class ["d-flex" "flex-column" "flex-md-row" "gap-3"]}]
+       (map feature-card card-data)))
+ 
 ;; Noj is an out-of-the-box Clojure library designed to streamline data science workflows for both newcomers and experienced users. Noj provides a tested and integrated collection of libraries that are known to work seamlessly together from day one, rather than requiring users to find, configure, and integrate multiple libraries separately. 
