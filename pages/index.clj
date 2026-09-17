93a327d8e320c96e3423cdaee61de805a81b4f6e
Cvetomir M. Dimov <cvetomir.dimov@unige.ch>
2026-09-17 15:36:44 +0200

    Small design tweaks

--- pages/index.clj
+++ pages/index.clj
@@ -4,40 +4,39 @@
    [scicloj.kindly.v4.api :as kindly]
    [scicloj.kindly.v4.kind :as kind]))
 
-
 ^:kindly/hide-code
 (kind/hiccup
  [:div
   {:class ["text-center"]}
-  [:h1 {:class ["text-center"]} "A Clojure data science toolkit"]
-  [:p "A tested and integrated collection of libraries that are known to work seamlessly together from day one."]
-  [:a {:href "quick_start_guide"} [:button {:type "button" :class ["btn" "btn-primary me-2"]} "Get started"]]
-  [:a {:href "learn/"} [:button {:type "button" :class ["btn" "btn-primary me-2" ]} "Learn Noj"]]])
+  [:h1 {:class ["text-center"]} "A Clojure Data Science Toolkit"]
+  [:p "A tested and integrated collection of libraries that are known to work seamlessly together from day one."] 
+  [:button {:type "button" :class ["btn" "btn-primary me-2"] :onclick "window.location.href=quick_start_guide"} "Get started"] ;; TODO: fix underlying
+  [:button {:type "button" :class ["btn" "btn-primary me-2"] :onclick "window.location.href=learn/"} "Learn Noj"]])
 
 ^:kindly/hide-code
 (defn feature-card [{:keys [heading text image]}]
   [:div {:class ["flex-fill" "w-100" "overflow-hidden" "p-2"]}
    [:div {:style {:display "flex" :align-items "center" :gap "12px"}}
-    [:h4 heading]]
-    [:p text]])
+    [:h3 heading]]
+    [:p text]]) ;; try making the font more visible 
 
 ^:kindly/hide-code
 (def features-data-row1
-  [{:heading "Simple"
-    :text "With Clojure, you can separate concerns."}
-   {:heading "Fast"
-    :text "The speed of Java and tech.ml.dataset."}
+  [{:heading "Fast"
+    :text "The speed of Java matched with fast dataset processing and low-overhead parallelism."}
    {:heading "Maintainable"
-    :text "With the backward compatibility of Clojure."}])
+    :text "No breaking changes, but accretion."}
+   {:heading "Simple"
+    :text "Reason easily about code by separating tangled concepts."}])
 
 ^:kindly/hide-code
 (def features-data-row2
-  [{:heading "Deploy easily"
-    :text "Use the same code in development and production."}
+  [{:heading "Rich Ecosystem"
+    :text "A complete data analysis ecosystem and direct access to mature Java libraries."}
+   {:heading "Easy Deployment"
+    :text "Use the same workflow in research and transition seamlessly to production."}
    {:heading "REPL"
-    :text "An unparalleled interactivity and in a quick feedback loop."}
-   {:heading "Uniform syntax"
-    :text ""}])
+    :text "A seamless feedback loop and runtime data manipulation."}])
 
 ^:kindly/hide-code
 (kind/hiccup [:br])
@@ -62,88 +61,103 @@
     :image "Clay.png"
     :heading "Exploring and Publishing"
     :content [:div
-              [:p [:a {:href "ref/clay"} "Clay"] " turns a namespace into a document, whether a website, a pdf or any other Quarto-supported document. It provides the notebook-oriented workflow familiar from Jupyter."]
-              [:p [:a {:href "ref/kindly"} "Kindly"] " supports Clay by specifying how forms are presented in the document. With over 30 different supported kinds, it is ready to populate your website with dynamic content."]]}
+              [:h4 [:a {:href "ref/clay"} "Clay"]]
+              [:p " publishes a namespace, whether as a website, a pdf or any other Quarto-supported document. It provides the notebook-oriented workflow familiar from Jupyter."]
+              [:h4 [:a {:href "ref/kindly"} "Kindly"]]
+              [:p " supports Clay by specifying how forms are presented in the document. With over 30 different supported kinds, it is ready to populate your website with dynamic content."]]}
    {:logo "Plotje.png"
     :short-heading "Visualize"
     :link "visualization"
     :image "Plotje.png"
     :heading "Data Visualization"
-    :content [:p [:a {:href "ref/plotje"} "Plotje"] " generates clear and compelling data visualizations. Inspired by the " [:a {:href "https://en.wikipedia.org/wiki/Wilkinson%27s_Grammar_of_Graphics"} "Grammer of Graphics"] " and Julia's " [:a {:href "aog.makie.org"} "Algebra of Graphics"] ", it enables the easy specification of complex plots in just a few lines of code."]}
+    :content [:div
+              [:h4 [:a {:href "ref/plotje"} "Plotje"]] 
+              [:p " generates clear and compelling data visualizations. Inspired by the " [:a {:href "https://en.wikipedia.org/wiki/Wilkinson%27s_Grammar_of_Graphics"} "Grammer of Graphics"] " and Julia's " [:a {:href "aog.makie.org"} "Algebra of Graphics"] ", it enables the easy specification of complex plots in just a few lines of code."]]}
    {:logo "TMD.png"
     :short-heading "HPC"
     :link "hpc"
     :image "TMD.png"
     :heading "High Performance Computing "
     :content [:div
-              [:p [:a {:href "ref/tech_ml_dataset"} "Tech.ml.dataset"] " is a high-performance dataset processing library providing a fast, Clojure-native alternative to R and Python's data frames."]
-              [:p [:a {:href "ref/ham_fisted"} "Ham-fisted"] " provides high-performance data structures and operations for Clojure."]
-              [:p [:a {:href "ref/dtype_next"} "Dtype-next"] " is a high-performance numeric array programming library. It provides a unified pathway for dealing with contiguous containers of primitive datatypes such as ints and floats on the JVM."]]}
+              [:h4 [:a {:href "ref/tech_ml_dataset"} "Tech.ml.dataset"]]
+              [:p " is a high-performance dataset processing library providing a fast, Clojure-native alternative to R and Python's data frames."]
+              [:h4 [:a {:href "ref/ham_fisted"} "Ham-fisted"]] 
+              [:p " provides high-performance data structures and operations for Clojure."]
+              [:h4 [:a {:href "ref/dtype_next"} "Dtype-next"]]
+              [:p " is a high-performance numeric array programming library. It provides a unified pathway for dealing with contiguous containers of primitive datatypes such as ints and floats on the JVM."]]}
    {:logo "TMD.png"
-    :short-heading "Process"
+    :short-heading "Process Data"
     :link "datasets"
     :heading "Data Processing"
     :content [:div
-              [:p "With " [:a {:href "ref/tablecloth"} "tablecloth"] ", you can manipulate datasets with the ergonomics of R's dplyr and the speed of " [:a {:href "ref/tech_ml_dataset"} "tech.ml.dataset"] ". "]
-              [:p [:a {:href "ref/tmd_parquet"} "TMD Parquet"] " provides parquet file format bindings bindings for tech.ml.dataset."]
-              [:p [:a {:href "ref/clojure_java_time"} "clojure.java-time"] " is a Java 8 Date-Time API wrapper for Clojure."]]}
+              [:h4 [:a {:href "ref/tablecloth"} "tablecloth"]]
+              [:p "With , you can manipulate datasets with the ergonomics of R's dplyr and the speed of " [:a {:href "ref/tech_ml_dataset"} "tech.ml.dataset"] ". "]
+              [:h4 [:a {:href "ref/tmd_parquet"} "TMD Parquet"]] 
+              [:p " provides parquet file format bindings bindings for tech.ml.dataset."]
+              [:h4 [:a {:href "ref/clojure_java_time"} "clojure.java-time"]]
+              [:p " is a Java 8 Date-Time API wrapper for Clojure."]]}
    {:logo "Fastmath.png"
     :short-heading "Stats"
     :link "math"
     :image "Fastmath.png"
     :heading "Mathematics and Statistics"
     :content [:div
-              [:p [:a {:href "ref/fastmath"} "Fastmath"] " is a comprehensive math and statistics library, which includes trigonometric and power functions, vector and matrix operations, procedures for integration and differentiation, distributions, statistical functions, and many more."]
-              [:p [:a {:href "ref/fitdistr"} "Fitdistr"] " provides an intuitive interface for fitting distributions, inspired by the famous R package."]
-              [:p [:a {:href "ref/ish"} "Same-ish"] " provides approximate numerical comparisons, which are useful for testing notebooks."]]}
+              [:h4 [:a {:href "ref/fastmath"} "Fastmath"]]
+              [:p " is a comprehensive math and statistics library, which includes trigonometric and power functions, vector and matrix operations, procedures for integration and differentiation, distributions, statistical functions, and many more."]
+              [:h4 [:a {:href "ref/fitdistr"} "Fitdistr"]]
+              [:p  " provides an intuitive interface for fitting distributions, inspired by the famous R package."]
+              [:h4 [:a {:href "ref/ish"} "Same-ish"]] 
+              [:p " provides approximate numerical comparisons, which are useful for testing notebooks."]]}
    {:logo "Metamorph.ml.png"
     :short-heading "ML"
     :link "ml"
     :image "Metamorph.ml.png"
     :heading "Machine Learning"
     :content [:div
-              [:p [:a {:href "ref/metamorph_ml"} "Metamorph.ml"] " is a platform for unified machine learning pipelines. It unifies hyperparameter tuning and data preprosessing in a single approach."]
-              [:p [:a {:href "ref/scicloj_ml_tribuo"} "Scicloj.ml.tribuo"] " is an integration of the established Java ML library " [:a {:href "https://tribuo.org/"} "Tribuo"] " into the metamorph framework."]
-              [:p [:a {:href "ref/sklearn_clj"} "Sklearn-clj"] " provides easy access to all models and estimators from " [:a {:href "https://scikit-learn.org/"} "scikit-learn"] " in Clojure."]]}
+              [:h4 [:a {:href "ref/metamorph_ml"} "Metamorph.ml"]]
+              [:p " is a platform for unified machine learning pipelines. It unifies hyperparameter tuning and data preprosessing in a single approach."]
+              [:h4 [:a {:href "ref/scicloj_ml_tribuo"} "Scicloj.ml.tribuo"]] 
+              [:p  " is an integration of the established Java ML library " [:a {:href "https://tribuo.org/"} "Tribuo"] " into the metamorph framework."]
+              [:h4 [:a {:href "ref/sklearn_clj"} "Sklearn-clj"]] 
+              [:p " provides easy access to all models and estimators from " [:a {:href "https://scikit-learn.org/"} "scikit-learn"] " in Clojure."]]}
    {:logo "Libpython_clj.png"
     :short-heading "Interop"
     :link "interop"
     :image "Libpython_clj.png"
     :heading "Bridges and Interop"
     :content [:div
-              [:p [:a {:href "ref/clojisr"} "ClojisR"] " is a Clojure bridge to R. With it, you can execute any R code within Clojure."]
-              [:p [:a {:href "ref/libpython_clj"} "Libpython_clj"] " is a deep integration between Python and Clojure. Python and Java objects are bridged and handled by the JVM GC."]
-              [:p [:a {:href "ref/kind_pyplot"} "Kind-pyplot"] " is a small Clojure library for displaying Python plots."]]}])
+              [:h4 {:class "green-heading"} [:a {:href "ref/clojisr"} "ClojisR"]] 
+              [:p " is a Clojure bridge to R. With it, you can execute any R code within Clojure."]
+              [:h4 [:a {:href "ref/libpython_clj"} "Libpython_clj"]] 
+              [:p " is a deep integration between Python and Clojure. Python and Java objects are bridged and handled by the JVM GC."]
+              [:h4 [:a {:href "ref/kind_pyplot"} "Kind-pyplot"]]
+              [:p " is a small Clojure library for displaying Python plots."]]}])
 
 ^:kindly/hide-code
 (defn library-active-tab [{:keys [short-heading logo link]}]
   [:li {:class "nav-item" :role "presentation"}
-   [:a {:class "nav-link active" :data-bs-toggle "tab" :href (str "#" link) :aria-selected "true" :role "tab"} [:img {:src logo :style {:width "1em" :height "auto"}}] " " [:b short-heading]]])
+   [:a {:class "nav-link active" :data-bs-toggle "tab" :href (str "#" link) :aria-selected "true" :role "tab"} [:img {:src logo :style {:width "1.1em" :height "auto"}}] " " [:b short-heading]]])
 
 ^:kindly/hide-code
 (defn library-tab [{:keys [short-heading logo link]}]
   [:li {:class "nav-item" :role "presentation"}
-   [:a {:class "nav-link" :data-bs-toggle "tab" :href (str "#" link) :aria-selected "true" :role "tab"} [:img {:src logo :style {:width "1em" :height "auto"}}] " " [:b short-heading]]])
+   [:a {:class "nav-link" :data-bs-toggle "tab" :href (str "#" link) :aria-selected "true" :role "tab"} [:img {:src logo :style {:width "1.1em" :height "auto"}}] " " [:b short-heading]]])
 
 ^:kindly/hide-code
 (defn library-tab-active-content [{:keys [heading content link image]}]
   [:div {:class "tab-pane fade show active" :id link :role "tabpanel"}
+   [:h3 {:class "text-center"} heading]
    [:div {:class "row d-flex flex-wrap"}
-    [:div {:class "col-6 w-50"}
-     [:h4 heading]
-     content]
-    #_[:div {:class "col-6 w-50"}
-       [:img {:src image}]]]])
+    [:div {:class "col-6 w-50"} content]
+    #_[:div {:class "col-6 w-50"} [:img {:src image}]]]])
 
 ^:kindly/hide-code
 (defn library-tab-content [{:keys [heading content link image]}]
   [:div {:class "tab-pane fade" :id link :role "tabpanel"}
+   [:h3 {:class "text-center"} heading]
    [:div {:class "row d-flex flex-wrap"}
-    [:div {:class "col-6 w-50"}
-     [:h4 heading]
-     content]
-    #_[:div {:class "col-6 w-50"}
-       [:img {:src image}]]]])
+    [:div {:class "col-6 w-50"} content]
+    #_[:div {:class "col-6 w-50"} [:img {:src image}]]]])
 
 ^:kindly/hide-code
 (kind/hiccup
@@ -170,10 +184,10 @@
    [:h1 "Community"]]
   [:div {:class "row d-flex gap-2"}
    [:div {:class "col-3 w-25"}
-    [:p  [:img {:src "SciCloj.png" :style {:width "1.2em" :height "auto"}}] " Noj was compiled and developed by the " [:a {:href "https://scicloj.github.io/"} "SciCloj"] " community. Join us!"]]
+    [:p  [:img {:src "SciCloj.png" :style {:width "1.2em" :height "auto"}}] " Noj was developed by the " [:a {:href "https://scicloj.github.io/"} "SciCloj"] " community. " [:a {:href "https://scicloj.github.io/docs/community/getting_involved/"} "Join us!"]]]
    [:div {:class "col-3 w-25"}
     [:p [:img {:src "zulip.png" :style {:width "1.5em" :height "auto"}}] " Check our " [:a  {:href "https://clojurians.zulipchat.com/#narrow/stream/321125-noj-dev"} "developer chat"] "."]]
    [:div {:class "col-3 w-25"}
-    [:p [:img {:src "github.png" :style {:width "1.2em" :height "auto"}}] " Check out the " [:a {:href "https://github.com/scicloj/noj"} "source code"] "."]]
+    [:p [:img {:src "github.png" :style {:width "1.2em" :height "auto"}}] " Study the " [:a {:href "https://github.com/scicloj/noj"} "source code"] "."]]
    [:div {:class "col-3 w-25"}
     [:p [:img {:src "zulip.png" :style {:width "1.5em" :height "auto"}}] " For any support, write us on the " [:a {:href "https://clojurians.zulipchat.com/#narrow/stream/151924-data-science"} " dedicated channel"] "."]]]])
